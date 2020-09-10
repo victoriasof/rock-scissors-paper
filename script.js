1e4a2dba7eee74b53cd6f4ab7b0f7cd3fd05114a
Victoria <victoria.sofianidou@gmail.com>
2020-09-10 12:25:40 +0200

    created html, css, js files, linked them. started editing html file with bootstrap, added images

---
+++ script.js
@@ -0,0 +1,7 @@
+/*
+Must-have features
+    Have three buttons that the player can press to change their pick
+    Have another button to let the computer pick one and show the winner
+    Let the player know they won (no alert box)
+    Have a reset button
+ */
